f66b6314cc46af964d81a1c7446763820dfee292
Dave Mandyam <77566595+dman96@users.noreply.github.com>
2021-02-21 03:25:25 -0500

    Update ArduinoLED.ino

--- ArduinoLED.ino
+++ ArduinoLED.ino
@@ -1,3 +1,4 @@
+/* This is the code that will be uploaded to the Arduino. In this example, the built-in LED light of the Arduino will be controlled. If there are external LEDs, then change the pins accordingly */
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -6,14 +7,16 @@ void setup() {
 void loop() {
   if (Serial.available()>0){
 
-    uint8_t c = "";
-    c = Serial.read();
+    //Read the incoming serial data into a char variable
+    char c = Serial.read();
 
+    //Turn on the LED if an "h" is received from the ESP32
     if (c == 'h'){
       Serial.println("LED is ON");
       digitalWrite(LED_BUILTIN, HIGH);
     }
 
+    //Turn off the LED if an "l" is received from the ESP32
     else if (c == 'l'){
       Serial.println("LED is OFF");
       digitalWrite(LED_BUILTIN, LOW);
